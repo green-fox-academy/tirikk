725a224ae4337cb3dd71866b3220ec1ce59838b8
Mate Matiovics <dontirikk@gmail.com>
2017-03-21 14:49:50 +0100

    add files for exercise{07..09}

---
+++ week-02/day-02/exercise-08/src/DefineBasicInfo.java
@@ -0,0 +1,19 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    // Define several things as a variable than print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Wether you are married or not as a boolean
+
+    String name = "Mate Matiovics";
+    int age = 28;
+    double height = 1.75;
+    boolean isMarried = false;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(isMarried);
+  }
+}
