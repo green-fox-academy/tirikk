d5382ead406201f9ff9c58108b7d483af37e9edb
Mate Matiovics <dontirikk@gmail.com>
2017-03-21 13:26:36 +0100

    add files for exercise{01..03}

---
+++ week-02/day-02/exercise-01/src/HelloMe.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Máté on 2017. 03. 21..
+ */
+public class HelloMe {
+    public static void main(String[] args) {
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, Máté!");
+    }
+}
---
+++ week-02/day-02/exercise-02/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello, Zita!");
+        System.out.println("Hello, Zandraa!");
+        System.out.println("Hello, Petra!");
+    }
+}
