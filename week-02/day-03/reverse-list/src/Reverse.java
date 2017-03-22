15cc414e0f34c6e281aebbe1276559e647639dd9
Mate Matiovics <dontirikk@gmail.com>
2017-03-22 21:06:49 +0100

    add files for 'Reverse list' exercise

---
+++ week-02/day-03/reverse-list/src/Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Reverse {
+  public static void main(String[] args) {
+
+    int[] aj = {3, 4, 5, 6, 7};
+
+    for (int i = 0; i < aj.length / 2; i++) {
+      int temp = aj[i];
+      aj[i] = aj[aj.length - i - 1];
+      aj[aj.length - i - 1] = temp;
+    }
+    System.out.println(Arrays.toString(aj));
+  }
+}
