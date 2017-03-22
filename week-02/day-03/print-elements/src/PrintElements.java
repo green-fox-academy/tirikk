7cdbf0900fb136a69d26d02891c8d4b49f1bdb1c
Mate Matiovics <dontirikk@gmail.com>
2017-03-22 15:11:18 +0100

    add files for 'Increment element' and 'Print elements' exercises

---
+++ week-02/day-03/print-elements/src/PrintElements.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class PrintElements {
+  public static void main(String[] args) {
+
+    int[] af = {4, 5, 6, 7};
+    System.out.println(Arrays.toString(af));
+  }
+}
