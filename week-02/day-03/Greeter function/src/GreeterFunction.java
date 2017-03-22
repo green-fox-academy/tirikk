a84070796291a14caba3dd8198fffcd0c5447014
Mate Matiovics <dontirikk@gmail.com>
2017-03-22 13:39:34 +0100

    add files for 'Greeter function' exercise

---
+++ week-02/day-03/Greeter function/src/GreeterFunction.java
@@ -0,0 +1,12 @@
+
+public class GreeterFunction {
+  public static void main(String[] args) {
+
+    String al = "Greenfox";
+    Greet(al);
+  }
+
+  public static void Greet(String a) {
+    System.out.println("Greetings dear, " + a);
+  }
+}
