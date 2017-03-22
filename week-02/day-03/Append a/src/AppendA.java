1e5158ac1a2dfc6eebe8ad3d6b444f1204fbae2b
Mate Matiovics <dontirikk@gmail.com>
2017-03-22 13:54:36 +0100

    add files for 'Append A' exercise

---
+++ week-02/day-03/Append a/src/AppendA.java
@@ -0,0 +1,12 @@
+public class AppendA {
+  public static void main(String[] args) {
+
+    String am = "kuty";
+    System.out.println(appendA(am));
+  }
+
+  public static String appendA(String input) {
+    String output = input + "a";
+    return output;
+  }
+}
