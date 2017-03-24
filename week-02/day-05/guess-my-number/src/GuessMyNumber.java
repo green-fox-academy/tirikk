212313fbd671bffba5de73d81b5b2cb1ac9e31bc
Mate Matiovics <dontirikk@gmail.com>
2017-03-24 11:12:35 +0100

    add random number generator function to 'Guess My Number' project

--- week-02/day-05/guess-my-number/src/GuessMyNumber.java
+++ week-02/day-05/guess-my-number/src/GuessMyNumber.java
@@ -3,6 +3,12 @@ import java.util.Random;
 public class GuessMyNumber {
   public static void main(String[] args) {
 
-    
+    System.out.println(randomGen(2, 10));
+  }
+
+  public static int randomGen(int min, int max) {
+    Random rand = new Random();
+    int number = rand.nextInt((max - min) + 1) + min;
+    return number;
   }
 }
