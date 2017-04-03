d8793626ee5e4a4f4ace25e7fdd385d5032960ef
Mate Matiovics <dontirikk@gmail.com>
2017-04-03 10:20:12 +0200

    add file for doable homework

---
+++ week-04/day-01/exercises/src/Trees.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class Trees {
+  public static void main(String[] args) {
+    HashMap<String, String> tree1 = new HashMap<>();
+    tree1.put("type", "beech");
+    tree1.put("leaf color", "green");
+    tree1.put("age", "176 years");
+    tree1.put("gender", "monoecious");
+
+    HashMap<String, String> tree2 = new HashMap<>();
+    tree1.put("type", "birch");
+    tree1.put("leaf color", "green");
+    tree1.put("age", "221 years");
+    tree1.put("gender", "monoecious");
+
+    HashMap<String, String> tree3 = new HashMap<>();
+    tree1.put("type", "maple");
+    tree1.put("leaf color", "green");
+    tree1.put("age", "98 years");
+    tree1.put("gender", "female");
+
+    HashMap<String, String> tree4 = new HashMap<>();
+    tree1.put("type", "oak");
+    tree1.put("leaf color", "green");
+    tree1.put("age", "445 years");
+    tree1.put("gender", "monoecious");
+
+    HashMap<String, String> tree5 = new HashMap<>();
+    tree1.put("type", "pine");
+    tree1.put("leaf color", "green");
+    tree1.put("age", "143 years");
+    tree1.put("gender", "monoecious");
+  }
+}
