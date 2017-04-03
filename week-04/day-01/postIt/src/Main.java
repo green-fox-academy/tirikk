fbfa88629aa5e31155d22ee4a664f8ae1ff73d44
Mate Matiovics <dontirikk@gmail.com>
2017-04-03 11:24:28 +0200

    remove doable homework and add files for 'PostIt' and 'BlogPost' exercises

---
+++ week-04/day-01/postIt/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    PostIt p1 = new PostIt();
+    p1.backgroundcolor = "orange";
+    p1.text = "Idea 1";
+    p1.textcolor = "blue";
+
+    PostIt p2 = new PostIt();
+    p1.backgroundcolor = "pink";
+    p1.text = "Awesome";
+    p1.textcolor = "black";
+
+    PostIt p3 = new PostIt();
+    p1.backgroundcolor = "yellow";
+    p1.text = "Superb!";
+    p1.textcolor = "green";
+  }
+}
---
+++ week-04/day-01/postIt/src/PostIt.java
@@ -0,0 +1,5 @@
+public class PostIt {
+  String backgroundcolor;
+  String text;
+  String textcolor;
+}
