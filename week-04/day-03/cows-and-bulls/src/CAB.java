e1d6e976240a4b186ca2ac5e6d3af71834bbbadb
Mate Matiovics <dontirikk@gmail.com>
2017-04-05 22:32:48 +0200

    refactor CAB class and add more methods

--- week-04/day-03/cows-and-bulls/src/CAB.java
+++ week-04/day-03/cows-and-bulls/src/CAB.java
@@ -1,35 +1,100 @@
-import java.util.Random;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 
 public class CAB {
-  private Integer randomNo;
+  private String randomNo;
   private String state;
   private int counter;
 
   CAB() {
-    Random random = new Random();
-    randomNo = random.nextInt(9000) + 1000;
+    randomGen();
     state = "playing";
-    counter = 0;
+    counter = 1;
   }
 
-  public Integer getRandom() {
+  public void randomGen() {
+    ArrayList<Integer> list = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      list.add(i);
+    }
+    Collections.shuffle(list);
+    randomNo = "";
+    if (list.get(0) != 0) {
+      for (int i : list.subList(0, 4)) {
+        randomNo += i;
+      }
+    } else {
+      for (int i : list.subList(1, 5)) {
+        randomNo += i;
+      }
+    }
+  }
+
+  public boolean checkIfValid (Integer input) {
+    if (input.toString().length() != 4) {
+      return false;
+    }
+    for (int i = 0; i < 3; i++) {
+      if (StringUtils.countMatches(input.toString(), input.toString().charAt(i)) > 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public void play() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Guess! (" + counter + "/8)");
+    Integer userInput = scanner.nextInt();
+
+    if (!checkIfValid(userInput)) {
+      System.out.println("Your guess must contain 4 different digits, and cannot start with 0!");
+      play();
+    } else if (isCorrect(userInput)) {
+      System.out.println("Correct!");
+      state = "finished";
+    } else if (counter == 8) {
+      System.out.println("Game over!");
+      state = "finished";
+    } else {
+      System.out.println(guess(userInput));
+      play();
+    }
+  }
+
+  public boolean isCorrect(Integer i) {
+    return i.toString().equals(randomNo);
+  }
+
+  public String guess(Integer i) {
+    counter++;
+    int bull = 0;
+    int cow = 0;
+    int index = 0;
+    for (Character c : i.toString().toCharArray()) {
+      if (c.equals(randomNo.charAt(index))) {
+        cow++;
+      } else if (randomNo.indexOf(c) >= 0) {
+        bull++;
+      }
+      index++;
+    }
+    return cow + " cow, " + bull + " bull";
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public String getRandomNo() {
     return randomNo;
   }
 
-  public void guess(Integer i) {
-    int bull = 0;
-    int cow = 0;
-    if (i.equals(randomNo)) {
-      System.out.println("Correct!");
-    } else {
-      for (Character c : i.toString().toCharArray()) {
-        if (c.equals(randomNo.toString().charAt(i.toString().indexOf(c)))) {
-          cow++;
-        } else if (randomNo.toString().indexOf(c) >= 0) {
-          bull++;
-        }
-      }
-      System.out.println(cow + " cow," + bull + " bull");
-    }
+  public int getCounter() {
+    return counter;
   }
 }
