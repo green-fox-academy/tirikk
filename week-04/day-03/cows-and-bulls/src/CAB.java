d3937980390a3c5ebb6d2886b235a7581b8f91f4
Mate Matiovics <dontirikk@gmail.com>
2017-04-05 17:35:02 +0200

    tweak class for Cows and Bulls exercise

--- week-04/day-03/cows-and-bulls/src/CAB.java
+++ week-04/day-03/cows-and-bulls/src/CAB.java
@@ -1,31 +1,31 @@
-import com.sun.xml.internal.fastinfoset.util.CharArray;
+import java.util.Random;
 
 public class CAB {
-  private Integer random;
+  private Integer randomNo;
   private String state;
   private int counter;
 
   CAB() {
-    random = 5698;
-//    random = (int) (Math.random() * 9999);
+    Random random = new Random();
+    randomNo = random.nextInt(9000) + 1000;
     state = "playing";
     counter = 0;
   }
 
   public Integer getRandom() {
-    return random;
+    return randomNo;
   }
 
   public void guess(Integer i) {
     int bull = 0;
     int cow = 0;
-    if (i.equals(random)) {
+    if (i.equals(randomNo)) {
       System.out.println("Correct!");
     } else {
       for (Character c : i.toString().toCharArray()) {
-        if (c.equals(random.toString().charAt(i.toString().indexOf(c)))) {
+        if (c.equals(randomNo.toString().charAt(i.toString().indexOf(c)))) {
           cow++;
-        } else if (random.toString().indexOf(c) >= 0) {
+        } else if (randomNo.toString().indexOf(c) >= 0) {
           bull++;
         }
       }
