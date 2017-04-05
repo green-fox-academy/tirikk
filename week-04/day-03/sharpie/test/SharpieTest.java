995ad82cc4fb91ea8587900f837d089000471fb0
Mate Matiovics <dontirikk@gmail.com>
2017-04-05 14:51:43 +0200

    add more testcases for Sharpie exercise

--- week-04/day-03/sharpie/test/SharpieTest.java
+++ week-04/day-03/sharpie/test/SharpieTest.java
@@ -9,4 +9,22 @@ class SharpieTest {
     green.use();
     assertEquals(90, green.inkAmount);
   }
+
+  @Test
+  void testColor() {
+    Sharpie green = new Sharpie("green", 20);
+    assertEquals("green", green.color);
+  }
+
+  @Test
+  void testWidth() {
+    Sharpie green = new Sharpie("green", 20);
+    assertEquals(20, green.width);
+  }
+
+  @Test
+  void testInkAmount() {
+    Sharpie green = new Sharpie("green", 20);
+    assertEquals(100, green.inkAmount);
+  }
 }
