90a1613f856d5d5cb89afbc67b078dc8fa20884c
Máté <dontirikk@gmail.com>
2017-04-17 18:50:54 +0200

    add fields and methods to Animal class

--- week-06/day-01/exercise-before-workshop/src/Animal.java
+++ week-06/day-01/exercise-before-workshop/src/Animal.java
@@ -1,2 +1,16 @@
 public abstract class Animal {
+  String name;
+  int age;
+  String gender;
+  boolean hasLegs;
+
+  void greet() {
+    System.out.println("Hello");
+  }
+
+  abstract void wantChild();
+
+  void eat() {
+    System.out.println("Eating...");
+  }
 }
