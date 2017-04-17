c7dcb3932442965b6bcc15c9e249f225b001b007
Máté <dontirikk@gmail.com>
2017-04-17 21:39:36 +0200

    add StringedInstrument class

---
+++ week-06/day-02/exercise-01/src/main/java/music/StringedInstrument.java
@@ -0,0 +1,6 @@
+package main.java.music;
+
+abstract class StringedInstrument extends Instrument {
+  int numberOfStrings;
+  final String FORMAT_FOR_PLAY = "%s, a %d-stringed instrument that %s\n";
+}
