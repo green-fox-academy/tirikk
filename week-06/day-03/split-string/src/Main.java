ffe190f5193257cecdf8701a71eb2ad2e3bb8b16
Máté <dontirikk@gmail.com>
2017-04-19 15:56:33 +0200

    add exception handling to Split String exercise

--- week-06/day-03/split-string/src/Main.java
+++ week-06/day-03/split-string/src/Main.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
 
+    System.out.println(Arrays.toString(stringSplitter("lovebarbara", 14)));
   }
 
-  public static String[] stringSplitter(String string, int index) {
+  private static String[] stringSplitter(String string, int index) {
     String[] stringArray = new String[2];
-    stringArray[0] = string.substring(0, index - 1);
-    stringArray[1] = string.substring(index, string.length() - 1);
+    try {
+      stringArray[0] = string.substring(0, index);
+      stringArray[1] = string.substring(index, string.length());
+    } catch (IndexOutOfBoundsException e) {
+      System.out.println("Index is out of bounds!");
+    }
     return stringArray;
   }
 }
