9a6e767b8bd2237eac3ccec76a647d907b5b0ac8
Máté <dontirikk@gmail.com>
2017-04-20 21:53:58 +0200

    change scope of LotteryFilter fields and methods

--- week-06/day-04/lotteryFilter/src/main/java/LotteryFilter.java
+++ week-06/day-04/lotteryFilter/src/main/java/LotteryFilter.java
@@ -7,37 +7,38 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class LotteryFilter {
-  List<String[]> lines;
-  List<String[]> filteredLines;
-  String source = "assets/otos.csv";
-  String destination = "assets/output.csv";
+class LotteryFilter {
+  private List<String[]> lines;
+  private List<String[]> filteredLines = new ArrayList<String[]>();
+  private String source = "assets/otos.csv";
+  private String destination = "assets/output.csv";
 
   void checkArguments(String[] args) {
     OptionParser parser = new OptionParser();
-    parser.accepts("-y").withRequiredArg();
-    parser.accepts("-f").withRequiredArg();
-    parser.accepts("-o").withRequiredArg();
+    parser.accepts("y").withRequiredArg();
+    parser.accepts("f").withRequiredArg();
+    parser.accepts("o").withRequiredArg();
     OptionSet options = parser.parse(args);
 
-    if (options.has("-y")) {
-      if (options.has("-f")) {
-        source = options.valueOf("-f").toString();
+    if (options.has("y")) {
+      if (options.has("f")) {
+        source = options.valueOf("f").toString();
       }
-      if (options.has("-o")) {
-        destination = options.valueOf("-o").toString();
+      if (options.has("o")) {
+        destination = options.valueOf("o").toString();
       }
       readFromFile();
-      filterLines(options.valueOf("-y").toString());
+      filterLines(options.valueOf("y").toString());
       writeToFile();
     } else {
       System.out.println("No year provided to filter by");
     }
   }
 
-  void filterLines(String year) {
+  private void filterLines(String year) {
     for (String[] line : lines) {
       if (line[0].equals(year)) {
         filteredLines.add(line);
@@ -45,7 +46,7 @@ public class LotteryFilter {
     }
   }
 
-  void readFromFile() {
+  private void readFromFile() {
     try {
       CSVReader reader = new CSVReader(new FileReader(source), ';');
       lines = reader.readAll();
@@ -56,7 +57,7 @@ public class LotteryFilter {
     }
   }
 
-  void writeToFile() {
+  private void writeToFile() {
     try {
       CSVWriter writer = new CSVWriter(new FileWriter(destination), ';', CSVWriter.NO_QUOTE_CHARACTER);
       writer.writeAll(filteredLines);
