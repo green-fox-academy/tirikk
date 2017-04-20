13fa8ec6218c306e54ab705a304ecd73b6348316
Máté <dontirikk@gmail.com>
2017-04-20 21:37:27 +0200

    add checkArguments method

--- week-06/day-04/lotteryFilter/src/main/java/LotteryFilter.java
+++ week-06/day-04/lotteryFilter/src/main/java/LotteryFilter.java
@@ -1,5 +1,7 @@
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,10 +11,43 @@ import java.util.List;
 
 public class LotteryFilter {
   List<String[]> lines;
+  List<String[]> filteredLines;
+  String source = "assets/otos.csv";
+  String destination = "assets/output.csv";
+
+  void checkArguments(String[] args) {
+    OptionParser parser = new OptionParser();
+    parser.accepts("-y").withRequiredArg();
+    parser.accepts("-f").withRequiredArg();
+    parser.accepts("-o").withRequiredArg();
+    OptionSet options = parser.parse(args);
+
+    if (options.has("-y")) {
+      if (options.has("-f")) {
+        source = options.valueOf("-f").toString();
+      }
+      if (options.has("-o")) {
+        destination = options.valueOf("-o").toString();
+      }
+      readFromFile();
+      filterLines(options.valueOf("-y").toString());
+      writeToFile();
+    } else {
+      System.out.println("No year provided to filter by");
+    }
+  }
+
+  void filterLines(String year) {
+    for (String[] line : lines) {
+      if (line[0].equals(year)) {
+        filteredLines.add(line);
+      }
+    }
+  }
 
   void readFromFile() {
     try {
-      CSVReader reader = new CSVReader(new FileReader("assets/otos.csv"), ';');
+      CSVReader reader = new CSVReader(new FileReader(source), ';');
       lines = reader.readAll();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
@@ -21,10 +56,10 @@ public class LotteryFilter {
     }
   }
 
-  void writeToFile(String path) {
+  void writeToFile() {
     try {
-      CSVWriter writer = new CSVWriter(new FileWriter("path"), ';', CSVWriter.NO_QUOTE_CHARACTER);
-      writer.writeAll(lines);
+      CSVWriter writer = new CSVWriter(new FileWriter(destination), ';', CSVWriter.NO_QUOTE_CHARACTER);
+      writer.writeAll(filteredLines);
       writer.close();
     } catch (IOException e) {
       e.printStackTrace();
--- week-06/day-04/lotteryFilter/src/main/java/Main.java
+++ week-06/day-04/lotteryFilter/src/main/java/Main.java
@@ -1,12 +1,7 @@
-import com.opencsv.CSVReader;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
 public class Main {
   public static void main(String[] args) {
 
+    LotteryFilter lotteryFilter = new LotteryFilter();
+    lotteryFilter.checkArguments(args);
   }
 }
