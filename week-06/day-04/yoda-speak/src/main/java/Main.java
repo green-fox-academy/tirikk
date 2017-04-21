aba0941faa08190730574cb3b461dd8a12845c4f
Máté <dontirikk@gmail.com>
2017-04-21 09:04:38 +0200

    add Scanner to ask for user input

--- week-06/day-04/yoda-speak/src/main/java/Main.java
+++ week-06/day-04/yoda-speak/src/main/java/Main.java
@@ -2,16 +2,24 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter a string");
+    String string = scanner.nextLine();
+
     try {
-      HttpResponse<String> response = Unirest.get("https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.")
+      HttpResponse<String> response = Unirest.get("https://yoda.p.mashape.com/yoda?sentence=" + string.replace(" ",
+              "+"))
               .header("X-Mashape-Key", "7xu0UJuSasmshpNhmLxirFzPoYHxp1Wf8CbjsnNR4O5fFCGYKZ")
               .header("Accept", "text/plain")
               .asString();
       System.out.println(response.getBody());
     } catch (UnirestException e) {
       e.printStackTrace();
-    };
+    }
   }
 }
