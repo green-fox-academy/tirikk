5855ab4b8ba641b5f1569871ea561987783978e1
Máté <dontirikk@gmail.com>
2017-05-02 12:06:39 +0200

    add AtomicLong field for 'REST Greet Counter App' exercise

--- week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  AtomicLong a1 = new AtomicLong();
 
   @RequestMapping("/greeting")
   public Greeting greeting(@RequestParam("name") String name) {
-    return new Greeting(1, name);
+    return new Greeting(a1.getAndIncrement(), "Hello, " + name);
   }
 }
