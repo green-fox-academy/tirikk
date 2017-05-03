98592201283a7240e78f31e134a9fedd7849a1cc
Máté <dontirikk@gmail.com>
2017-05-03 13:22:40 +0200

    add changes for exercise05

--- week-07/day-03/bankaccount/src/main/java/com/greenfox/controllers/BankAccountController.java
+++ week-07/day-03/bankaccount/src/main/java/com/greenfox/controllers/BankAccountController.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BankAccountController {
+  List<BankAccount> bankAccounts = new ArrayList<>();
 
   @RequestMapping("/Exercise1")
   public String firstEndpoint(Model model) {
@@ -19,5 +23,15 @@ public class BankAccountController {
   public String secondEndpoint(Model model) {
     model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
     return "secondEndpoint";
+  }
+
+  @RequestMapping("/Exercise5")
+  public String thirdEndpoint(Model model) {
+    bankAccounts.add(new BankAccount("Simba", "2000", "lion"));
+    bankAccounts.add(new BankAccount("Timon", "1000", "meerkat"));
+    bankAccounts.add(new BankAccount("Pumba", "3000", "warthog"));
+    bankAccounts.add(new BankAccount("Mufasa", "5000", "lion"));
+    model.addAttribute("bankAccounts", bankAccounts);
+    return "thirdEndpoint";
   }
 }
