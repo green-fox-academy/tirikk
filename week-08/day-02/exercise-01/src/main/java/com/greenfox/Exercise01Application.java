14ed4b9951b0d1b6beec9fbaee712f3f1040c5d0
Máté <dontirikk@gmail.com>
2017-05-09 14:22:57 +0200

    connect to mysql

--- week-08/day-02/exercise-01/src/main/java/com/greenfox/Exercise01Application.java
+++ week-08/day-02/exercise-01/src/main/java/com/greenfox/Exercise01Application.java
@@ -1,23 +1,13 @@
 package com.greenfox;
 
-import com.greenfox.model.Todo;
-import com.greenfox.repository.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Exercise01Application implements CommandLineRunner {
-	@Autowired
-	TodoRepository todoRepo;
+public class Exercise01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Exercise01Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		todoRepo.save(new Todo("I have to learn Object Relational Mapping"));
-	}
 }
