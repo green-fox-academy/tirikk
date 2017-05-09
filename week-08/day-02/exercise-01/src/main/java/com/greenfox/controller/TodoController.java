6473fe007da5b19d6d0dd9cd071d98bae4662cbd
Máté <dontirikk@gmail.com>
2017-05-09 15:56:55 +0200

    add some stuff with try-catch, just for the fun of it

--- week-08/day-02/exercise-01/src/main/java/com/greenfox/controller/TodoController.java
+++ week-08/day-02/exercise-01/src/main/java/com/greenfox/controller/TodoController.java
@@ -1,11 +1,15 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.Todo;
 import com.greenfox.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/todo")
@@ -15,8 +19,21 @@ public class TodoController {
 
   @RequestMapping(value = {"/", "/list"})
   public String list(Model model, @RequestParam(name = "isActive", required = false) Boolean isActive) {
-    if (isActive) {
-      model.addAttribute("todoRepo", todoRepo.fi);
+    try {
+      if (isActive) {
+        Iterable<Todo> list = todoRepo.findAll();
+        List<Todo> undone = new ArrayList<>();
+        for (Todo todo : list) {
+          if (!todo.isDone()) {
+            undone.add(todo);
+          }
+        }
+        model.addAttribute("todoRepo", undone);
+      } else {
+        model.addAttribute("todoRepo", todoRepo.findAll());
+      }
+    } catch (NullPointerException e) {
+      model.addAttribute("todoRepo", todoRepo.findAll());
     }
     return "todo";
   }
