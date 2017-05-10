7e5e7e5d09759a8f6bd8a12232cfd17f3ecb99e3
Máté <dontirikk@gmail.com>
2017-05-10 11:17:42 +0200

    init commit for day-03 workshop

---
+++ week-08/day-03/workshop/src/main/java/com/greenfox/Day03WorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day03WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day03WorkshopApplication.class, args);
+	}
+}
