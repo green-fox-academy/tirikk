d9eac5763494036151e9dd8ec14257c9924770e5
Mate Matiovics <dontirikk@gmail.com>
2017-07-10 09:41:17 +0200

    create transformList method

---
+++ week-16/exam/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+
+    System.out.println(transformList(Arrays.asList("abc", "ijk", "xyz")));
+  }
+
+  private static List<String> transformList(List<String> inputList) {
+    List<String> outputList = new ArrayList<>();
+    for (int i = 0; i < inputList.size(); i++) {
+      StringBuilder builder = new StringBuilder("");
+      for (String string : inputList) {
+        builder.append(string.charAt(i));
+      }
+      outputList.add(builder.toString());
+    }
+    return outputList;
+  }
+}
